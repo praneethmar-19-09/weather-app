ea4335ca6885bafe6b46ab231212402c80bbcd5e
praneethm3 <praneethm@radware.com>
2025-12-16 23:05:06 +0530

    feat: set up initial app structure and environment configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+const App: React.FC = () => {
+
+  return (
+    <>
+        <h1>Hello world!</h1>
+    </>
+  );
+};
+
+export default App;
--- src/config/env.ts
+++
@@ -1,4 +0,0 @@
-export const ENV = {
-  WEATHER_API_KEY: import.meta.env.VITE_WEATHER_API_KEY,
-  WEATHER_BASE_URL: "https://api.openweathermap.org/data/2.5",
-};
